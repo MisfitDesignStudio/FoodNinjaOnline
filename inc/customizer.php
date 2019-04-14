9fafb98f0678fd2b0ba439fdb4e20dbb35dbc0f2
Mario <mario@misfitdesign.hr>
2019-04-14 15:38:44 +0200

    First commit

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Food Ninja Online Theme Customizer
+ *
+ * @package Food_Ninja_Online
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function foodninjaonline_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'foodninjaonline_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'foodninjaonline_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'foodninjaonline_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function foodninjaonline_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function foodninjaonline_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function foodninjaonline_customize_preview_js() {
+	wp_enqueue_script( 'foodninjaonline-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'foodninjaonline_customize_preview_js' );
