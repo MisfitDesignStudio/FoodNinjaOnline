18fe7066ff7b80bb3ed9e5e17597481c5e06d39c
Mario <mario@misfitdesign.hr>
2019-04-14 17:16:03 +0200

    container and section padding

--- header.php
+++ header.php
@@ -12,8 +12,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,38 +22,24 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'foodninjaonline' ); ?></a>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'foodninjaonline'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
+
+			</div><!-- .site-branding -->
+
+			<nav id="site-navigation" class="main-navigation">
 				<?php
-			else :
+				wp_nav_menu(array(
+					'theme_location'	=> 'main-menu',
+					'menu_id'					=> 'primary-menu',
+					'container'				=> false,
+					'items_wrap'			=> '<ul class="menu-list">%3$s</ul>',
+				));
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$foodninjaonline_description = get_bloginfo( 'description', 'display' );
-			if ( $foodninjaonline_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $foodninjaonline_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'foodninjaonline' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+		<div id="content" class="site-content">
